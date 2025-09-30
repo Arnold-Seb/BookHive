8a500be71904877e3fbb6dafebc29e7b13601eb3
Ahnaf Shahriar Dip <ahnafshahriardip@gmail.com>
2025-09-30 20:29:27 +1000

    Updated DB schema for Borrow

--- src/models/Borrow.js
+++ src/models/Borrow.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
+// This model has been updated...
 const BorrowSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   studentName: { type: String, required: true },
+
+
   book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
   bookTitle: { type: String, required: true },
   quantity: { type: Number, default: 1 },
   borrowedAt: { type: Date, default: Date.now },
+
   dueDate: { type: Date, required: true },
   returned: { type: Boolean, default: false },
   overdueNotified: { type: Boolean, default: false }, // for overdue email
